07bafb0df1a70669f6e0ae6a6ec98f91f1c27787
Damian Gul <damian.robert.gul@gmail.com>
2021-04-06 16:40:41 +0200

    Added the possibility of accepting the order by the admin

---
+++ Projekt/Controllers/ZlecenieAdminController.cs
@@ -0,0 +1,184 @@
+﻿using Projekt.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Projekt.Controllers
+{
+    public class ZlecenieAdminController : Controller
+    {
+        
+        string connectionString = @"Data Source=.;Initial Catalog=Projekt;Integrated Security=True";
+
+        public ActionResult Index()
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                string query = "SELECT z.opis, z.waga, z.ilosc, k.imie, k.nazwisko, k.telefon, z.id " +
+                    "FROM zlecenie z INNER JOIN KONTO ko " +
+                    "ON z.konto_id = ko.id INNER JOIN klient k " +
+                    "ON ko.id = k.konto_id";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.Fill(dataTable);
+
+            }
+            return View(dataTable);
+        }
+
+        // GET: ZlecenieAdmin/Details/5
+        public ActionResult Details(int id)
+        {
+            DataTable dataTable = new DataTable();
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "SELECT a.ulica, a.numer, a.miasto, a.kod " +
+                    "FROM adres a INNER JOIN klient k " +
+                    "ON a.id = k.adres_id INNER JOIN KONTO ko " +
+                    "ON ko.id = k.konto_id INNER JOIN zlecenie z " +
+                    "ON z.konto_id = ko.id " +
+                    "WHERE z.id = @ZlecenieID;";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@ZlecenieID", id);
+                sqlDa.Fill(dataTable);
+            }
+            return View(dataTable);
+        }
+
+        //// GET: ZlecenieAdmin/Create
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: ZlecenieAdmin/Create
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: ZlecenieAdmin/Edit/5
+        public ActionResult Edit(int id)
+        {
+            DodajZlecenieModel dodajZlecenieModel = new DodajZlecenieModel();
+            DataTable dtZlecenia = new DataTable();
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                string query = "SELECT k.id, z.opis, z.waga, z.ilosc " +
+                    "FROM klient k INNER JOIN KONTO ko " +
+                    "ON ko.id = k.konto_id INNER JOIN zlecenie z " +
+                    "ON z.konto_id = ko.id " +
+                    "WHERE z.id = @ZlecenieID";
+                SqlDataAdapter sqlDa = new SqlDataAdapter(query, sqlCon);
+                sqlDa.SelectCommand.Parameters.AddWithValue("@ZlecenieID", id);
+
+                sqlDa.Fill(dtZlecenia);
+
+            }
+
+            if (dtZlecenia.Rows.Count == 1)
+            {
+                dodajZlecenieModel.KlientID = Convert.ToInt32(dtZlecenia.Rows[0][0].ToString());
+                dodajZlecenieModel.ZlecenieOpis = dtZlecenia.Rows[0][1].ToString();
+                dodajZlecenieModel.ZlecenieWaga = Convert.ToInt32(dtZlecenia.Rows[0][2].ToString());
+                dodajZlecenieModel.ZlecenieIlosc = Convert.ToInt32(dtZlecenia.Rows[0][3].ToString());
+
+                return View(dodajZlecenieModel);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // POST: ZlecenieAdmin/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, DodajZlecenieModel dodajZlecenieModel)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+
+                string query = "INSERT INTO przesyłka(pracownik_id, klient_id,samochod_id, data_przewozu, cena_netto_przewozu, dlugosc_trasy) " +
+                    "VALUES(@PracownikID, @KlientID, @SamochodID, @DataPrzewozu, @CennaZaKm, @DlugoscTrasy);";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@PracownikID", dodajZlecenieModel.PracownikID);
+                sqlCmd.Parameters.AddWithValue("@KlientID", dodajZlecenieModel.KlientID);
+                sqlCmd.Parameters.AddWithValue("@SamochodID", dodajZlecenieModel.SamochodID);
+                sqlCmd.Parameters.AddWithValue("@DataPrzewozu", dodajZlecenieModel.DataPrzewozu);
+                sqlCmd.Parameters.AddWithValue("@CennaZaKm", dodajZlecenieModel.CennaZaKm);
+                sqlCmd.Parameters.AddWithValue("@DlugoscTrasy", dodajZlecenieModel.DlugoscTrasy);
+                sqlCmd.ExecuteNonQuery();
+
+                query = "SELECT id FROM przesyłka " +
+                    "WHERE dlugosc_trasy = @DlugoscTrasy and " +
+                    "cena_netto_przewozu = @CennaZaKm and " +
+                    "data_przewozu = @DataPrzewozu";
+                sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@DataPrzewozu", dodajZlecenieModel.DataPrzewozu);
+                sqlCmd.Parameters.AddWithValue("@CennaZaKm", dodajZlecenieModel.CennaZaKm);
+                sqlCmd.Parameters.AddWithValue("@DlugoscTrasy", dodajZlecenieModel.DlugoscTrasy);
+                int id_przesylka = Convert.ToInt32(sqlCmd.ExecuteScalar());
+
+                query = "INSERT INTO towar_do_przewiezienia VALUES(@PrzesylkaID, @ZlecenieOpis, @ZlecenieWaga, @ZlecenieIlosc);";
+                sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@PrzesylkaID", id_przesylka);
+                sqlCmd.Parameters.AddWithValue("@ZlecenieOpis", dodajZlecenieModel.ZlecenieOpis);
+                sqlCmd.Parameters.AddWithValue("@ZlecenieWaga", dodajZlecenieModel.ZlecenieWaga);
+                sqlCmd.Parameters.AddWithValue("@ZlecenieIlosc", dodajZlecenieModel.ZlecenieIlosc);
+                sqlCmd.ExecuteNonQuery();
+
+                //TODO USUNIE SIE TO TERAZ Z ZLECENIE I KLIENT BEDZIE MIAL ZAKLADKE OGARNIETE ZLECENIA
+
+                query = "DELETE FROM zlecenie WHERE id=@ZlecenieID";
+                sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.Parameters.AddWithValue("@ZlecenieID", id);
+                sqlCmd.ExecuteNonQuery();
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: ZlecenieAdmin/Delete/5
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: ZlecenieAdmin/Delete/5
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ Projekt/Models/DodajZlecenieModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Projekt.Models
+{
+    public class DodajZlecenieModel
+    {
+        [DisplayName("Podaj ID pracownika")]
+        public int PracownikID { get; set; }
+        [DisplayName("Podaj ID  klienta")]
+        public int KlientID { get; set; }
+        [DisplayName("Podaj ID samochodu")]
+        public int SamochodID { get; set; }
+        [DisplayName("Podaj datę przyjęcia zamówienia")]
+        public DateTime DataPrzewozu { get; set; }
+        [DisplayName("Podaj cenę za kilometr")]
+        public int CennaZaKm { get; set; }
+        [DisplayName("Podaj długość trasy")]
+        public int DlugoscTrasy { get; set; }
+        [DisplayName("Podaj opis przesyłki")]
+        public string ZlecenieOpis { get; set; }
+        [DisplayName("Podaj wagę przesyłki")]
+        public int ZlecenieWaga { get; set; }
+        [DisplayName("Podaj ilość")]
+        public int ZlecenieIlosc { get; set; }
+    }
+}
