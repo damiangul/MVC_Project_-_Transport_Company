3819bf29a87523344d0f0c7b61df60b385e771fa
Damian Gul <damian.robert.gul@gmail.com>
2021-04-24 10:51:24 +0200

    Add validation to registration

--- Projekt/Controllers/HomeController.cs
+++ Projekt/Controllers/HomeController.cs
@@ -109,66 +109,57 @@ namespace Projekt.Controllers
         [HttpPost]
         public ActionResult RegisterAccount(KlientModel klientModel)
         {
-            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            if(ModelState.IsValid)
             {
-                sqlCon.Open();
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
 
-                string query = "INSERT INTO adres VALUES(@KlientUlica, @KlientNumer, @KlientMiasto, @KlientKod);";
-                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.Parameters.AddWithValue("@KlientUlica", klientModel.KlientUlica);
-                sqlCmd.Parameters.AddWithValue("@KlientNumer", klientModel.KlientNumer);
-                sqlCmd.Parameters.AddWithValue("@KlientMiasto", klientModel.KlientMiasto);
-                sqlCmd.Parameters.AddWithValue("@KlientKod", klientModel.KlientKod);
-                sqlCmd.ExecuteNonQuery();
+                    string query = "INSERT INTO adres VALUES(@KlientUlica, @KlientNumer, @KlientMiasto, @KlientKod);";
+                    SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@KlientUlica", klientModel.KlientUlica);
+                    sqlCmd.Parameters.AddWithValue("@KlientNumer", klientModel.KlientNumer);
+                    sqlCmd.Parameters.AddWithValue("@KlientMiasto", klientModel.KlientMiasto);
+                    sqlCmd.Parameters.AddWithValue("@KlientKod", klientModel.KlientKod);
+                    sqlCmd.ExecuteNonQuery();
 
-                query = "SELECT id FROM adres" +
-                    " WHERE ulica=@KlientUlica and numer=@KlientNumer";
-                sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.Parameters.AddWithValue("@KlientUlica", klientModel.KlientUlica);
-                sqlCmd.Parameters.AddWithValue("@KlientNumer", klientModel.KlientNumer);
-                int id_adres = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    query = "SELECT id FROM adres" +
+                        " WHERE ulica=@KlientUlica and numer=@KlientNumer";
+                    sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@KlientUlica", klientModel.KlientUlica);
+                    sqlCmd.Parameters.AddWithValue("@KlientNumer", klientModel.KlientNumer);
+                    int id_adres = Convert.ToInt32(sqlCmd.ExecuteScalar());
 
-                query = "INSERT INTO KONTO VALUES(@KlientLogin, @KlientHaslo, 'user');";
-                sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.Parameters.AddWithValue("@KlientLogin", klientModel.KlientLogin);
-                sqlCmd.Parameters.AddWithValue("@KlientHaslo", klientModel.KlientHaslo);
-                sqlCmd.ExecuteNonQuery();
+                    query = "INSERT INTO KONTO VALUES(@KlientLogin, @KlientHaslo, 'user');";
+                    sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@KlientLogin", klientModel.KlientLogin);
+                    sqlCmd.Parameters.AddWithValue("@KlientHaslo", klientModel.KlientHaslo);
+                    sqlCmd.ExecuteNonQuery();
 
-                query = "SELECT id from KONTO" +
-                    " WHERE login_user=@KlientLogin and password_user=@KlientHaslo";
-                sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.Parameters.AddWithValue("@KlientLogin", klientModel.KlientLogin);
-                sqlCmd.Parameters.AddWithValue("@KlientHaslo", klientModel.KlientHaslo);
-                int id_konto = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                    query = "SELECT id from KONTO" +
+                        " WHERE login_user=@KlientLogin and password_user=@KlientHaslo";
+                    sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@KlientLogin", klientModel.KlientLogin);
+                    sqlCmd.Parameters.AddWithValue("@KlientHaslo", klientModel.KlientHaslo);
+                    int id_konto = Convert.ToInt32(sqlCmd.ExecuteScalar());
 
 
-                query = "INSERT INTO klient VALUES(@id_adres, @id_konto, @KlientImie, @KlientNazwisko, @KlientTelefon, @KlientEmail)";
-                sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.Parameters.AddWithValue("@id_adres", id_adres);
-                sqlCmd.Parameters.AddWithValue("@id_konto", id_konto);
-                sqlCmd.Parameters.AddWithValue("@KlientImie", klientModel.KlientImie);
-                sqlCmd.Parameters.AddWithValue("@KlientNazwisko", klientModel.KlientNazwisko);
-                sqlCmd.Parameters.AddWithValue("@KlientTelefon", klientModel.KlientTelefon);
-                sqlCmd.Parameters.AddWithValue("@KlientEmail", klientModel.KlientEmail);
-                sqlCmd.ExecuteNonQuery();
+                    query = "INSERT INTO klient VALUES(@id_adres, @id_konto, @KlientImie, @KlientNazwisko, @KlientTelefon, @KlientEmail)";
+                    sqlCmd = new SqlCommand(query, sqlCon);
+                    sqlCmd.Parameters.AddWithValue("@id_adres", id_adres);
+                    sqlCmd.Parameters.AddWithValue("@id_konto", id_konto);
+                    sqlCmd.Parameters.AddWithValue("@KlientImie", klientModel.KlientImie);
+                    sqlCmd.Parameters.AddWithValue("@KlientNazwisko", klientModel.KlientNazwisko);
+                    sqlCmd.Parameters.AddWithValue("@KlientTelefon", klientModel.KlientTelefon);
+                    sqlCmd.Parameters.AddWithValue("@KlientEmail", klientModel.KlientEmail);
+                    sqlCmd.ExecuteNonQuery();
+                }
+
+                return RedirectToAction("Login", "Home");
+            } else
+            {
+                return View(klientModel);
             }
-
-            return RedirectToAction("Login", "Home");
         }
-
-        //[Authorize(Roles="admin")]
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
-
-        //    return View();
-        //}
-
-        //public ActionResult Contact()
-        //{
-        //    ViewBag.Message = "Your contact page.";
-
-        //    return View();
-        //}
     }
 }
